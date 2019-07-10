8bfebe6925e71bd98abd694e64077e5f202f4685
mc-santiago <mari.c.santiago@europe.com>
2019-07-10 10:23:39 +0200

    another commit

--- src/AdminComponents/AdminUsuarios.js
+++ src/AdminComponents/AdminUsuarios.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import AddUsuario from './AddUsuario';
-import { Container, Header, Segment, Feed, Icon } from "semantic-ui-react";
+import { Container, Header, Segment, Feed, Icon, Divider} from "semantic-ui-react";
 
 
 export default class AdminUsuarios extends Component {
+    constructor (props){
+        super(props);
+        this.state={
+            usuarios: []
+        }
+        this.loadUsuarios()
+    }
+
+    loadUsuarios = () => {
+        fetch('/api/users')
+        .then (res => res.json())
+        .then (data => this.setState({usuarios:data}))
+    }
     render() {
         return (
                 <Container fluid={true}>  
@@ -12,22 +25,25 @@ export default class AdminUsuarios extends Component {
                 </Header> 
                 <Segment raised >
                 <Feed>
+                <Header as='h3'>Listado de Usuarios</Header>
+                 <Divider/>
+                    {this.state.usuarios.map(usuario => 
                     <Feed.Event>
                         <Feed.Label>
                             <Icon name='user' size='massive' />
                         </Feed.Label>
                         <Feed.Content>
-                            Nombre Apellido 
+                            {usuario.name} es usuario(a) de EarnEat y {usuario.email} es el correo electrónico que utiliza en esta plataforma. 
                             <Feed.Meta>
                                 <Icon name='trash' />
                                 ¿Eliminar usuario?
                             </Feed.Meta>
                         </Feed.Content>
-                    </Feed.Event>
+                    </Feed.Event>)}
                 </Feed>
                 </Segment>
                 <Segment raised >
-                    <AddUsuario/>
+                    <AddUsuario onAddUsuario={this.loadUsuarios}/>
                 </Segment>
                 </Container>
         )
