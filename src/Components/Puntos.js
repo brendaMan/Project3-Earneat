eb72615faa95668aed3eb6d6be59efc9f616269e
mc-santiago <mari.c.santiago@europe.com>
2019-07-15 11:14:38 +0200

    Area de Puntos ya terminada con sus fetch y props... falta un poco de CSS y ya.

--- src/Components/Puntos.js
+++ src/Components/Puntos.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import AccionesRecientes from './AccionesRecientes';
-import { Header, Container, Divider, Form, Segment, TextArea, Input, Select} from 'semantic-ui-react';
+import { Header, Container, Divider, Form, Segment, TextArea, Input, Modal, Icon, Button, Select} from 'semantic-ui-react';
 
 export default class Puntos extends Component {
     constructor(props){
         super(props);
         this.state = {
+            user: props.user,
             de_usuario_id: props.user.id,
             a_usuario_id: 0,
             usuarios: [],
             puntos: "",
             razon: "",
             message:"", 
-            feed: []
+            feed: [], 
+            visible: false
         };
         fetch('/api/dropdown/usuarios') 
         .then(res => res.json())
@@ -33,63 +35,91 @@ export default class Puntos extends Component {
         })
         .then(res => {
             if (res.status === 200) {
-                this.setState({message : 'Voto completado'})
+                this.setState({message : `Tu regalo de ${this.state.puntos} puntos ha sido completado.`, visible: true})
                 this.loadFeed();
             }
-            else this.setState({message: 'There is a problem'})
+            else this.setState({message: 'Lo siento, pero tu regalo no se ha completado. Verifica que hayas completado el formulario. Si el problema persiste, contacta a algun administrador.', visible: true})
         })
-        .catch (err => this.setState({message: 'There is a problem'}))
-    }   
+        .catch (err => this.setState({message: 'Lo siento, pero tu regalo no se ha completado. Verifica que hayas completado el formulario. Si el problema persiste, contacta a algun administrador.', visible: true}))
+    }  
+    onClose = () => this.setState({ visible: false })
     
     render() {
         return (
-            <Container fluid={true}>  
-                <Header 
-                    as='h2'     
-                    id='headerContainer' 
-                    block
-                    inverted color="teal"
-                >
-                    Dashboard
-                </Header> 
+            <Container  
+                fluid={true}
+                className='containerPuntos' 
+            >
+            <Header 
+                as='h2'     
+                id='headerContainer' 
+                block
+                inverted color="teal"
+            >
+                Dashboard
+            </Header> 
 {/* Segment con el Form para regalar puntos */}
             <Segment raised>
-                { (this.state.message) ? 
-                    <p>{this.state.message}</p>
-                    :
-                    <Form fluid="fluid" widths='equal'>
+                    <Form 
+                        fluid="fluid" 
+                    >
                         <Header 
                             as='h3' 
                             color="teal">
                             Regalar Puntos
                         </Header>
                         <Divider/>
-    {/* Inputs para el Form */}
-                        <Form.Group inline>
-                            De ###{} puntos, quiero dar 
-                        <Input
-                            value={this.state.puntos} 
-                            onChange={e => this.setState({puntos: e.target.value})} /> 
-                        puntos a 
-                        <Select 
-                            options={this.state.usuarios} 
-                            onChange={(e, data) => this.setState({a_usuario_id: data.value})} />
+{/* Inputs para el Form */}
+                        <Form.Group 
+                            className='linea1Puntos'
+                            grouped
+                        >
+                            De  <b>{this.props.a_regalar}</b>  puntos, quiero dar 
+                            <Input
+                                icon='star'
+                                iconPosition='left'
+                                className='inputPuntos'
+                                value={this.state.puntos} 
+                                onChange={e => this.setState({puntos: e.target.value})}
+                            /> 
+                            puntos a  
+                            <Select
+                                className='selectPuntos' 
+                                options={this.state.usuarios} 
+                                onChange={(e, data) => this.setState({a_usuario_id: data.value})} />
                         </Form.Group>
                         <Form.Group 
+                            className='linea2Puntos'
+                            grouped
                             value={this.state.razon} 
-                            onChange={e => this.setState({razon: e.target.value})}>
+                            onChange={e => this.setState({razon: e.target.value})}
+                        >
                             por: <TextArea></TextArea>
                         </Form.Group>
-    {/* Button para regalar puntos */}
-                        <Form.Button 
-                            align='center'
-                            onClick={this.onRegalar}>
-                            Regalar
-                        </Form.Button>
-    {/* Mensaje de Error */}
-                        <p style={{ color: 'red'}}>{this.state.message}</p>
+{/* Button para regalar puntos */}
+                        <Container 
+                            textAlign='right'
+                        >
+                            <Button 
+                                onClick={this.onRegalar}>
+                                Regalar
+                            </Button>
+                        </Container>
+                        <Modal
+                            size={"large"}
+                            open={this.state.visible}
+                            close={this.onClose}
+                        >
+                            <Modal.Content>
+                                <p>{this.state.message}</p>
+                            </Modal.Content>
+                            <Modal.Actions>
+                                <Button color='green' onClick={this.onClose} inverted>
+                                    <Icon name='checkmark' /> Got it
+                                </Button>
+                            </Modal.Actions>
+                        </Modal>
                     </Form>
-                }
             </Segment> 
 {/* Componente del Newsfeed  */}
             <AccionesRecientes feed={this.state.feed} />
