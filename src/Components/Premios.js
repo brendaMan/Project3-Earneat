bb4704ceb3f3b3738291f36ce3d18992616ac807
mc-santiago <mari.c.santiago@europe.com>
2019-07-17 19:11:49 +0200

    done?

--- src/Components/Premios.js
+++ src/Components/Premios.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Header, Container, Button, Card, Modal, Icon, Image, Segment,} from 'semantic-ui-react';
+import { Header, Container, Button, Card, Modal, Icon, Image, Segment, Divider} from 'semantic-ui-react';
 import imagen_premio from './imagenes/imagen_premio.png';
 
 
@@ -47,40 +47,33 @@ export default class Premios extends Component {
 
   
   render() {
-    const premios = this.state.premios;
-    const user = this.state.user;
-    
-
+      const premios = this.state.premios;
+      const user = this.state.user;
     return(
-        <div>
-          <Container fluid className ='containerAll'>
-               <Segment>
-                 <Header as='h2' block>
-                 {/*premios.length*/}  PREMIOS
-                 </Header>
-              </Segment>
-          
-            
-          <Segment>
-            <Segment>
-                 <Header as='h2' block textAlign='center'>
-                   ¡ Enhorabuena {user.nombre}, tienes acumulados {this.props.saldo} puntos !
-                 </Header>
-            </Segment>
-          
-            <Card.Group centered >
-             {premios.map (premio => 
-               <Card>
-                <Image 
-                  className="ui medium image"
-                  src = {imagen_premio}
-                 wrapped ui={false} 
-                />
-               <Card.Content>
-                 <Card.Header className='premio-header' textAlign= 'center' >{premio.nombre}</Card.Header>
-                 <Card.Meta className='date' textAlign= 'center'>
-                      <span>{premio.puntos} Puntos </span>
-                  </Card.Meta>
+      <Container className='containerAll' fluid={true} >
+        <Header as='h2' id='headerContainer' block inverted color="teal">
+          Premios
+        </Header>
+{/* Header con el saldo de puntos que tienes para comprar.  */}
+        <Segment raised>
+          <Header as='h3' block textAlign='center' color='teal'>
+             ¡ Enhorabuena {user.nombre}, tienes acumulados {this.props.saldo} puntos !
+          </Header>
+        <Divider/>
+{/* Tarjetas con la informacion de cada premio. */}
+        <Card.Group centered >
+         {premios.map (premio => 
+          <Card>
+            <Image
+              size='tiny'
+              src = {imagen_premio}
+              wrapped ui={false} 
+            />
+            <Card.Content>
+              <Card.Header className='premio-header' textAlign= 'center' >{premio.nombre}</Card.Header>
+               <Card.Meta className='date' textAlign= 'center'>
+                  <span>{premio.puntos} Puntos </span>
+                </Card.Meta>
                 <Card.Description  className= 'descripcion'  textAlign='center'>{premio.descripcion}</Card.Description>
                </Card.Content>
                
@@ -88,33 +81,26 @@ export default class Premios extends Component {
                   <Button color='teal' onClick={() => this.onCanjear(premio)} >
                      Me lo quedo
                   </Button>
-                   <Modal
-                        size={"large"}
-                        open={this.state.visible}
-                        close={this.onClose}
-                        >
-                      <Modal.Content>
-                         <p>{this.state.message}</p>
-                            </Modal.Content>
-                            <Modal.Actions>
-                                <Button color='green' onClick={this.onClose} inverted>
-                                    <Icon name='checkmark' /> OK
-                                </Button>
-                            </Modal.Actions>
-                        </Modal>
-
+{/* Mensaje */}
+                  <Modal 
+                    centered={false} size='mini'
+                    open={this.state.visible}
+                    close={this.onClose}
+                  >
+                    <Modal.Content>
+                      <p>{this.state.message}</p>
+                    </Modal.Content>
+                    <Modal.Actions>
+                      <Button color='green' onClick={this.onClose} inverted>
+                        <Icon name='checkmark' /> OK
+                      </Button>
+                    </Modal.Actions>
+                  </Modal>
               </Card.Content>
-
              </Card>)  }
-
             </Card.Group>
-             
          </Segment>
-     
         </Container>
-            
-            
-    </div>
           );
        };
              
